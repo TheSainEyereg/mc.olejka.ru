83487a403c8e81931776025d2ec5c6ebb1469171
TheSainEyereg <oleglogwinow@gmail.com>
2022-08-02 22:48:51 +0300

    Got rid of my old lib

--- assets/main.js
+++ assets/main.js
@@ -1,54 +1,45 @@
 const domain = "mc.olejka.ru";
-theme.current = "dark";
 
-ready(_ => {
-	const overlay = el("#overlay"), footer = el("footer");
+document.querySelector("footer span").textContent = new Date().getFullYear();
 
-	footer.innerHTML = `&copy;Copyright ${new Date().getFullYear()} olejka.ru<a href="https://status.olejka.ru/" target="blank">Status</a>`
+const overlay = document.querySelector("#overlay");
+fetch(`https://api.mcsrvstat.us/2/${domain}`).then(async res => {
+	const info = await res.json();
 
-    data.get(`https://api.mcsrvstat.us/2/${domain}`, m => {
-        if (m.online) {
-            overlay.remove();
+	if (info.online) {
+		overlay.remove();
 
-            el("#icon").setAttribute("src", m.icon || "/assets/icons/noicon.png");
-            el("#motd").innerHTML = m.motd.html;
-            el("#version").innerHTML = m.version;
+		document.querySelector("#icon").src = info.icon || "/assets/icons/noicon.png";
+		document.querySelector("#motd").innerHTML = info.motd.html;
+		document.querySelector("#version").textContent = info.version;
 
-            const connect =  el("#connect");
-            const data = `${domain}:${m.port}`;
-            connect.innerHTML = data;
-            connect.onclick = _ => {
-                const form = document.createElement("input");
-                form.setAttribute("type", "text");
-                form.id = "copyPlace";
-                document.body.append(form);
-                form.value = data;
-                form.select();
-                document.execCommand("copy");
-                form.remove();
-                connect.classList.add("copied");
-                connect.innerHTML = "Copied!";
-                setTimeout(_ => {
-                    connect.classList.remove("copied");
-                    connect.innerHTML = data;
-                }, 500)
-            };
-            
-            if (m.players.online>0) {
-                el("#count").innerHTML = `Online ${m.players.online}/${m.players.max}:`;
-            } else {
-                el("#count").innerHTML = `No one is online :(`;
-            };
-            
-            for(let i in m.players.list) {
-                const li = document.createElement("li");
-                li.innerHTML = m.players.list[i];
-                el("#list").append(li);
-            };
-        } else {
-            overlay.children[0].innerHTML = "Offline<br><span>Can't connect!</span>";
-            overlay.classList.remove("loading");
-            overlay.classList.add("offline");
-        }
-    }, "status");
+		const connect = document.querySelector("#connect");
+		const data = `${domain}:${info.port}`;
+		connect.textContent = data;
+		connect.onclick = async _ => {
+			await navigator.clipboard.writeText(data);
+
+			connect.classList.add("copied");
+			connect.innerHTML = "Copied!";
+
+			setTimeout(_ => {
+				connect.classList.remove("copied");
+				connect.innerHTML = data;
+			}, 500)
+		};
+		
+		const count = document.querySelector("#count");
+		count.textContent = info.players.online > 0 ? `Online ${info.players.online}/${info.players.max}:` : `No one is online :(`;
+
+		info.players.online && info.players.list.forEach(player => {
+			const li = document.createElement("li");
+			li.textContent = player;
+			document.querySelector("#list").append(li);
+		});
+
+	} else {
+		overlay.children[0].innerHTML = "Offline<br><span>Can't connect!</span>";
+		overlay.classList.remove("loading");
+		overlay.classList.add("offline");
+	}
 })
